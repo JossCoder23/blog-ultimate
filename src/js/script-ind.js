909f179cfdfc7afdd6e8b48a90056de31d33771c
Jose Pardo <jose.pardo@upch.pe>
2024-12-21 11:58:07 -0500

    update11582112

--- src/js/script-ind.js
+++ src/js/script-ind.js
@@ -8,4 +8,13 @@ window.addEventListener("load", () => {
         indexContent.classList.toggle('bloque6LeftIndexBlock');
     })
 
+    document.getElementById('boton-compartir').addEventListener("click", function() {
+        var metaTag = document.createElement('meta');
+        metaTag.property = "og:image";
+        metaTag.content = "https://blogs.cayetano.edu.pe/pregrado/bioincuba/";
+        document.head.appendChild(metaTag);
+        const url = 'https://www.facebook.com/sharer/sharer.php?i=https%3A%2F%2Flp.cayetano.edu.pe%2Fwp-content%2Fuploads%2F2024%2F12%2Fimage-nota-24.jpg&u=https%3A%2F%2Fblogs.cayetano.edu.pe%2Fpregrado%2Fbioincuba%2F'
+        window.open(url, '_blank')
+    })
+
 })
